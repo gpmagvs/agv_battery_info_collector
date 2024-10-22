751712934a7b1da0fc5fed32a774cff48498294a
jinweihuang20 <jinweihuang1020@gmail.com>
2024-10-22 13:19:04 +0800

    post 數據改為另開執行續+while迴圈定時post / 新增 .launch

--- ReadMe.md
+++ ReadMe.md
@@ -1,37 +1,65 @@
-# AGV电池信息采集器
+# 使用方式
+## 安裝
+1. 將專案資料夾放置到 ROS 工作空間的 src 目錄中：
+   如果您已經下載了agv_battery_info_collector資料夾，直接將其複製到 `~/catkin_ws/src` 目錄中：
 
-该脚本从自动导引车（AGV）收集电池信息并将其发送到指定的服务器端点。它订阅ROS消息并将数据发布到RESTful API。
+2. 返回 ROS 工作空間的根目錄，並執行 catkin_make 來編譯套件：
 
-## 功能
+   ```bash
+   cd ~/catkin_ws
+   catkin_make
+   ```
 
-- 使用ROS消息收集AGV的电池信息。
-- 将收集到的数据发布到指定的RESTful API端点。
-- 通过命令行参数进行配置。
+3. 修改啟動檔案參數
 
-## 要求
+   打開 `launch/agv_battery_info_collector.launch` 檔案，根據需要修改以下參數：
 
-- Python 3.x
-- ROS（机器人操作系统）
-- `rospy` 包
-- 包含 `ModuleInformation` 和 `BatteryState` 消息的 `gpm_msgs` 包
+   - `host`: 伺服器地址和端口，預設為 "192.168.0.3:5254"
+   - `agvName`: AGV 名稱，預設為 "AGV_001"
+   - `fieldName`: 區域名稱，預設為 "Unknown_Region"
+   - `pubInterval`: 資料發布間隔（秒），預設為 10
 
-## 安装
+   例如：
 
-1. 确保已安装Python 3和ROS。
-2. 安装必要的ROS包：
-    ```bash
-    sudo apt-get install ros-<your-ros-distro>-rospy
-    sudo apt-get install ros-<your-ros-distro>-gpm-msgs
-    ```
-3. 克隆此仓库并导航到项目目录：
-    ```bash
-    git clone <repository_url>
-    cd <repository_directory>
-    ```
+   ```xml
+   <arg name="host" default="192.168.1.100:5000"/>
+   <arg name="agvName" default="AGV_002"/>
+   <arg name="fieldName" default="Warehouse_A"/>
+   <arg name="pubInterval" default="5"/>
+   ```
 
-## 使用
+4. 啟動節點
 
-使用所需参数运行脚本：
+   使用以下指令啟動 AGV 電池資訊收集器節點：
 
-```bash
-./battery_info_collector.py --fieldName <字段名称> --agvName <AGV名称> --host <主机URL>
+   ```
+   roslaunch agv_battery_info_collector agv_battery_info_collector.launch
+   ```
+
+   如果需要覆蓋預設參數，可以在啟動指令中指定：
+
+   ```
+   roslaunch agv_battery_info_collector agv_battery_info_collector.launch host:=192.168.1.100:5000 agvName:=AGV_002 fieldName:=Warehouse_A pubInterval:=5
+   ```
+
+啟動後，節點將訂閱 `/module_information` 主題，收集電池資訊並按照指定的間隔將資料發送到伺服器。
+
+
+
+
+5. 將啟動指令添加到 Shell 腳本
+
+   如果您使用 Shell 腳本來啟動多個程序(例如車載電腦中的agvc.sh)，請將 AGV 電池資訊收集器的啟動指令添加到您的腳本中。例如，您可以在腳本中添加以下行：
+
+   ```bash
+   roslaunch agv_battery_info_collector agv_battery_info_collector.launch
+   ```
+
+   如果需要使用自定義參數，可以這樣寫：
+
+   ```bash
+   roslaunch agv_battery_info_collector agv_battery_info_collector.launch host:=192.168.1.100:5000 agvName:=AGV_002 fieldName:=Warehouse_A pubInterval:=5
+   ```
+
+   確保將此行添加到適當的位置，以便與其他程序一起啟動。
+
--- scripts/agv-battery-info-collector.py
+++ scripts/agv-battery-info-collector.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 import rospy
 import http.client
+import socket
 import json
 import argparse
+import threading
 from gpm_msgs.msg import ModuleInformation,BatteryState
 
+latest_battery_data = None
 def post_request(url, endpoint, headers, data):
 
     parsed_url = url.replace('http://', '').replace('https://', '')
@@ -25,52 +28,81 @@ def post_request(url, endpoint, headers, data):
     return response.status, response_data
 
 
-def PostBatInfoToServer(batinfo, fieldName, agvName,host):
+def PostBatInfoToServer(batinfo, fieldName, agvName, host):
     
     headers = {'Content-Type': 'application/json'}
     try:
-        status, response_data = post_request("http://{}".format(host),"/api/AGVBattery?fieldName={}&agvName={}".format(fieldName,agvName),headers,batinfo)
+        status, response_data = post_request("http://{}".format(host), "/api/AGVBattery?fieldName={}&agvName={}".format(fieldName, agvName), headers, batinfo)
         #print(response_data)
+        
+        return status == 200  # Return True if the request was successful (HTTP 200), otherwise False
 	
     except Exception as e:
         rospy.logerr("HTTP request failed: {}".format(e))
         return False
 
-def ModuleInformationCallback(moduleinfo, args):
-    fieldName, agvName,host = args
-    data={
-        "batteryID":moduleinfo.Battery.batteryID,
-        "state":moduleinfo.Battery.state,
-        "errorCode":moduleinfo.Battery.errorCode,
-        "Voltage":moduleinfo.Battery.Voltage,
-        "batteryLevel":moduleinfo.Battery.batteryLevel,
-        "chargeCurrent":moduleinfo.Battery.chargeCurrent,
-        "dischargeCurrent":moduleinfo.Battery.dischargeCurrent,
-        "maxCellTemperature":moduleinfo.Battery.maxCellTemperature,
-        "minCellTemperature":moduleinfo.Battery.minCellTemperature,
-        "cycle":moduleinfo.Battery.cycle,
-        "chargeTime":moduleinfo.Battery.chargeTime,
-        "useTime":moduleinfo.Battery.useTime,
-        "tagNumber":moduleinfo.reader.tagID
-        }
-    PostBatInfoToServer(data, fieldName, agvName,host)
+def ModuleInformationCallback(moduleinfo):
+    data = {
+        "batteryID": moduleinfo.Battery.batteryID,
+        "state": moduleinfo.Battery.state,
+        "errorCode": moduleinfo.Battery.errorCode,
+        "Voltage": moduleinfo.Battery.Voltage,
+        "batteryLevel": moduleinfo.Battery.batteryLevel,
+        "chargeCurrent": moduleinfo.Battery.chargeCurrent,
+        "dischargeCurrent": moduleinfo.Battery.dischargeCurrent,
+        "maxCellTemperature": moduleinfo.Battery.maxCellTemperature,
+        "minCellTemperature": moduleinfo.Battery.minCellTemperature,
+        "cycle": moduleinfo.Battery.cycle,
+        "chargeTime": moduleinfo.Battery.chargeTime,
+        "useTime": moduleinfo.Battery.useTime,
+        "tagNumber": moduleinfo.reader.tagID
+    }
+    global latest_battery_data
+    latest_battery_data = data
 
-
-def battery_info_collector(fieldName, agvName,host):
+def check_host_connection(host):
+    try:
+        socket.gethostbyname(host)
+        return True
+    except socket.error:
+        return False
+def battery_info_collector():
     rospy.init_node('battery_info_collector', anonymous=True)
-    #rospy.Subscriber('battery_level', Float32, battery_callback)
-    rospy.Subscriber('/module_information', ModuleInformation, ModuleInformationCallback, callback_args=(fieldName, agvName,host))
+    host = rospy.get_param('~host', 'localhost')
+    agvName = rospy.get_param('~agvName', 'AGV1')
+    fieldName = rospy.get_param('~fieldName', 'Unknown_Region')
+    pubInterval = rospy.get_param('~pubInterval', 10)
+    rospy.loginfo("Starting battery_info_collector with host: {}, agvName: {}, fieldName: {}, pubInterval: {}".format(host, agvName, fieldName, pubInterval))
+    
+
+    rospy.Subscriber('/module_information', ModuleInformation, ModuleInformationCallback)
     rospy.loginfo("Battery info collector node started.")
+
+    if not check_host_connection(host):
+        rospy.logwarn("Failed to connect to host: {}. Test communication status failed.".format(host))
+
+    def publish_battery_data(fieldName, agvName,host,pubInterval):
+        global latest_battery_data
+        while not rospy.is_shutdown():
+                if latest_battery_data:
+                    rospy.logwarn("PostBatInfoToServer ...")
+                    success = PostBatInfoToServer(latest_battery_data,fieldName,agvName,host)
+                    if not success:
+                        rospy.logwarn("PostBatInfoToServer failed...")
+                    rospy.sleep(pubInterval)
+                else:
+                    rospy.logwarn("No battery data received yet...")
+                    rospy.sleep(3)
+
+    # Start the publishing thread
+    thread = threading.Thread(target=publish_battery_data, args=(fieldName, agvName,host,pubInterval,))
+    thread.daemon = True  # This ensures the thread will exit when the main program does
+    thread.start()
+
     rospy.spin()
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Battery info collector for AGV.')
-    parser.add_argument('--fieldName', type=str, required=True, help='Field name for the API request')
-    parser.add_argument('--agvName', type=str, required=True, help='AGV name for the API request')
-    parser.add_argument('--host', type=str, required=True, help='HOST URL for the API request')
-    args = parser.parse_args()
-    rospy.loginfo(args)
-    try:
-        battery_info_collector(args.fieldName,args.agvName,args.host)
+    try:        
+        battery_info_collector()
     except rospy.ROSInterruptException:
         pass
